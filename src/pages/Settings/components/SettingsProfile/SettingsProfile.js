86ff99534341f9adfe6627450cea53f850098f4b
Nelte Vreeke <nvreeke@student.codam.nl>
2019-11-19 16:53:17 +0100

    basic form setup for update user

--- src/pages/Settings/components/SettingsProfile/SettingsProfile.js
+++ src/pages/Settings/components/SettingsProfile/SettingsProfile.js
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './SettingsProfile.scss'
+import request from 'utils/request'
+import FormUpdateUser from './components/FormUpdateUser/FormUpdateUser'
+
+const getErrorMessage = error => {
+  const { message } = error.body
+
+  if (message === 'conflict') {
+    return 'That email already exists'
+  }
+
+  return 'Something went wrong, please try again later'
+}
 
 const SettingsProfile = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [submitError, setSubmitError] = useState('')
+
+  const handleFormSubmit = async ({ firstName, lastName, email, password, age, gender, biography }) => {
+    setIsSubmitting(true)
+
+    try {
+      await request('signup', {
+        method: 'POST',
+        body: {
+          firstName,
+          lastName,
+          email,
+          age,
+          gender,
+          password,
+          biography
+        }
+      })
+    } catch (e) {
+      setSubmitError(getErrorMessage(e))
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
   return (
     <div className={styles.component}>
-      Profile
+      <FormUpdateUser
+        onSubmit={handleFormSubmit}
+        isSubmitting={isSubmitting}
+        error={submitError}
+      />
     </div>
   )
 }
---
+++ src/pages/Settings/components/SettingsProfile/components/FormUpdateUser/FormUpdateUser.js
@@ -0,0 +1,147 @@
+import React from 'react'
+import styles from './FormUpdateUser.scss'
+import { useSelector } from 'react-redux'
+// import * as Yup from 'yup'
+import * as Gender from 'constants/Gender'
+
+import {
+  Formik,
+  Form
+} from 'formik'
+
+import {
+  Field,
+  Input,
+  Select,
+  Textarea
+} from 'components/FormElements'
+
+import { getUser } from 'selectors/user'
+import Button from '../../../../../../components/Button/Button'
+
+const setInitialValues = (user) => {
+  return {
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    age: user.age,
+    gender: user.gender,
+    password: '',
+    biography: ''
+  }
+}
+
+const genderOptions = [{
+  label: 'Male',
+  value: Gender.MALE
+}, {
+  label: 'Female',
+  value: Gender.FEMALE
+}]
+
+const FormUpdateUser = ({
+  onSubmit,
+  isSubmitting,
+  error
+}) => {
+  const user = useSelector(getUser)
+  const initialValues = setInitialValues(user)
+
+  return (
+    <div className={styles.component}>
+      <Formik
+        // validationSchema={validationSchema}
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => {
+          return (
+            <Form>
+              <Field
+                label='First name'
+                id='firstName'
+                name='firstName'
+                type='text'
+                component={Input}
+              />
+
+              <Field
+                label='Last name'
+                id='lastName'
+                name='lastName'
+                type='text'
+                component={Input}
+              />
+
+              <Field
+                label='Email'
+                id='email'
+                name='email'
+                type='email'
+                component={Input}
+              />
+
+              <Field
+                label='Age'
+                id='age'
+                name='age'
+                type='number'
+                min='18'
+                max='99'
+                component={Input}
+              />
+
+              <Field
+                label='Gender'
+                id='gender'
+                name='gender'
+                component={Select}
+                options={genderOptions}
+              />
+
+              <Field
+                label='Password'
+                id='password'
+                name='password'
+                component={Input}
+                type='password'
+              />
+
+              <Field
+                label='Repeat password'
+                id='repeatPassword'
+                name='passwordRepeat'
+                component={Input}
+                type='password'
+              />
+
+              <Field
+                label='Biography'
+                id='biography'
+                name='biography'
+                component={Textarea}
+              />
+
+              {error &&
+                <div
+                  className={styles.submitError}
+                >
+                  {error}
+                </div>}
+
+              <Button
+                type='submit'
+                onClick={handleSubmit}
+                variant={Button.VARIANT_DEFAULT}
+                isLoading={isSubmitting}
+              >
+                  update
+              </Button>
+            </Form>
+          )
+        }}
+      />
+    </div>
+  )
+}
+
+export default FormUpdateUser
