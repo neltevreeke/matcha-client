f00bf9c470c72f566030fc0c54debd0aa59e8dd8
Nelte Vreeke <nvreeke@student.codam.nl>
2019-11-26 17:13:01 +0100

    rendering standard user icon when someone didnt upload a photo yet

--- src/pages/Dashboard/components/PotentialMatches/PotentialMatches.js
+++ src/pages/Dashboard/components/PotentialMatches/PotentialMatches.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
-// import styles from './PotentialMatches.scss'
+import styles from './PotentialMatches.scss'
 import { getCloudinaryUrlFromPublicId } from 'utils/cloudinary'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { potentialMatches } from 'actions/users'
@@ -40,19 +41,28 @@ const PotentialMatches = () => {
           'q_100'
         ])
 
-        // return this if url = true
-        // else return a standard user icon
-        return (
+        {return url ? (
           <div
             key={index}
             style={{
               backgroundImage: `url(${url})`,
               height: '140px',
               width: '140px',
-              borderRadius: '3px'
+              borderRadius: '3px',
+              cursor: 'pointer'
             }}
           />
-        )
+        ) : (
+          <div
+            key={index}
+            className={styles.noPhotoProfile}
+          >
+            <FontAwesomeIcon
+              className={styles.icon}
+              icon='user'
+            />
+          </div>
+        ) }
       })}
     </>
   )
