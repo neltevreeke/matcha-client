14f83b1ba51a0d9947ea724850748ab77275b890
Nelte Vreeke <nvreeke@student.codam.nl>
2020-01-13 18:28:57 +0100

    Now scrolling into view on chat load or new message

--- src/pages/Matches/components/Chat/ChatMessages/ChatMessages.js
+++ src/pages/Matches/components/Chat/ChatMessages/ChatMessages.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './ChatMessages.scss'
 import { useSelector } from 'react-redux'
 import { getUser } from '../../../../../selectors/user'
@@ -10,55 +10,70 @@ const ChatMessages = ({
 }) => {
   const user = useSelector(getUser)
   const onlineUsers = useSelector(getOnlineUsers)
+  const messagesEndRef = useRef(null)
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({
+      behavior: 'smooth'
+    })
+  }
+
+  useEffect(
+    scrollToBottom,
+    [messages]
+  )
 
   return (
-    <div className={styles.component}>
-      {messages.map((message, index) => {
-        const isOnline = onlineUsers.some(onlineUser => onlineUser._id === message.createdBy)
+    <>
+      <div className={styles.component}>
+        {messages.map((message, index) => {
+          const isOnline = onlineUsers.some(onlineUser => onlineUser._id === message.createdBy)
 
-        if (message.createdBy !== user._id) {
-          return (
-            <div
-              className={styles.matchMessage}
-              key={index}
-            >
+          if (message.createdBy !== user._id) {
+            return (
               <div
-                className={styles.matchAvatar}
-              >
-                <Avatar
-                  user={message.createdBy}
-                  isOnline={isOnline}
-                />
-              </div>
-              <p
+                className={styles.matchMessage}
                 key={index}
               >
+                <div
+                  className={styles.matchAvatar}
+                >
+                  <Avatar
+                    user={message.createdBy}
+                    isOnline={isOnline}
+                  />
+                </div>
+                <p
+                  key={index}
+                >
+                  {message.content}
+                </p>
+              </div>
+            )
+          }
+
+          return (
+            <div
+              key={index}
+              className={styles.userMessage}
+            >
+              <p>
                 {message.content}
               </p>
+              <div
+                className={styles.userAvatar}
+              >
+                <Avatar
+                  user={user}
+                  isOnline
+                />
+              </div>
             </div>
           )
-        }
-
-        return (
-          <div
-            key={index}
-            className={styles.userMessage}
-          >
-            <p>
-              {message.content}
-            </p>
-            <div
-              className={styles.userAvatar}
-            >
-              <Avatar
-                user={user}
-                isOnline
-              />
-            </div>
-          </div>
-        )
-      })}
-    </div>
+        })}
+        <div ref={messagesEndRef} />
+      </div>
+    </>
   )
 }
 
