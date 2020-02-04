8c133815cb341221dc9e378ba2e6d6a16a3ec62f
neltevreeke <nelte.p.vreeke@gmail.com>
2020-02-04 15:52:43 +0100

    location will still be used, even when user blocks navigator.geolocation

--- src/utils/location.js
+++ src/utils/location.js
@@ -1,8 +1,19 @@
+import request from 'utils/request'
+
 export const getPosition = () => new Promise(resolve => {
   navigator.geolocation.getCurrentPosition((position) => {
     resolve({
       lat: position.coords.latitude,
       long: position.coords.longitude
+    })
+  }, async () => {
+    const { lat, long } = await request('location', {
+      method: 'GET'
+    })
+
+    resolve({
+      lat,
+      long
     })
   })
 })
