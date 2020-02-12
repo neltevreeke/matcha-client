0c1c4ac05b5d9c49fa3f24cf5ca60b87f5bae0d4
neltevreeke <nelte.p.vreeke@gmail.com>
2020-02-12 16:25:59 +0100

    Refactor on the interest tags, you can no longer add your own but only choose from a pre-defined list of interests

--- src/pages/Settings/components/SettingsInterestTags/SettingsInterestTags.js
+++ src/pages/Settings/components/SettingsInterestTags/SettingsInterestTags.js
@@ -1,38 +1,67 @@
 import React from 'react'
 import styles from './SettingsInterestTags.scss'
-import FormAddInterestTag from './FormAddInterestTag/FormAddInterestTag'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { update } from 'actions/users'
-import {
-  getUpdateIsLoading,
-  getUser
-} from 'selectors/user'
+import { getUser } from 'selectors/user'
+
+const predefinedInterests = [{
+  label: 'Golf'
+}, {
+  label: 'Music Production'
+}, {
+  label: '3D printing'
+}, {
+  label: 'Acting'
+}, {
+  label: 'Aeromodeling'
+}, {
+  label: 'Air sports'
+}, {
+  label: 'Airbrushing'
+}, {
+  label: 'Airsoft'
+}, {
+  label: 'Beadwork'
+}, {
+  label: 'Beatboxing'
+}, {
+  label: 'BMX'
+}, {
+  label: 'Bowling'
+}, {
+  label: 'Boxing'
+}, {
+  label: 'Collecting'
+}, {
+  label: 'Curling'
+}]
 
 const SettingsInterestTags = () => {
-  const updateIsLoading = useSelector(getUpdateIsLoading)
   const user = useSelector(getUser)
   const dispatch = useDispatch()
 
-  const handleFormSubmit = ({ tag }, { resetForm }) => {
-    // check for uniqueness
-
-    const interests = [
-      ...user.interests,
-      {
-        label: tag
-      }
-    ]
+  const handleOnClickTag = index => () => {
+    const interests = user.interests.filter(interest => user.interests[index].label !== interest.label)
 
     dispatch(update({
       interests
     }))
-
-    resetForm()
   }
 
-  const handleOnClickTag = index => () => {
-    const interests = user.interests.filter(interest => user.interests[index].label !== interest.label)
+  const handleOnClickPredefinedInterest = (label) => () => {
+    const uniqueTag = user.interests.some(i => i.label === label)
+
+    if (uniqueTag) {
+      return
+    }
+
+    const interests = [
+      ...user.interests,
+      {
+        label
+      }
+    ]
 
     dispatch(update({
       interests
@@ -54,12 +83,21 @@ const SettingsInterestTags = () => {
           )
         })}
       </div>
-      <div className={styles.inputComponent}>
-        <FormAddInterestTag
-          onSubmit={handleFormSubmit}
-          isSubmitting={updateIsLoading}
-          // error={error}
-        />
+      <div className={styles.component}>
+        <p>Click on an interests to add it to your interests</p>
+        <div className={styles.tagContainer}>
+          {predefinedInterests.map((interest, index) => {
+            return (
+              <div
+                key={index}
+                className={styles.predefinedInterest}
+                onClick={handleOnClickPredefinedInterest(interest.label)}
+              >
+                #{interest.label}
+              </div>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
